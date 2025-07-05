5927f00650972f6501c0a4f042c93440b0b857b0
Ayana-Kudo <kudo.ayana.akt@gmail.com>
2025-07-05 16:34:18 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ const upload = multer();
 
 app.use(cors());
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true, limit:'10mb' }));
 app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 const filePath = path.join(__dirname, "responses.csv");
 
@@ -28,7 +29,8 @@ if (!fs.existsSync(filePath)) {
     "q1",
     "q2",
     "q3",
-    "q4"
+    "q4",
+    "imageFile"
   ];
   fs.writeFileSync(filePath, "\uFEFF" + header.join(",") + "\n", "utf8");
 }
@@ -36,6 +38,8 @@ if (!fs.existsSync(filePath)) {
 // データ受け取り用エンドポイント
 app.post("/submit", upload.none(), (req, res) => {
   const now = new Date().toISOString();
+  const timestamp = Date.now(); // 画像ファイル用
+
   const {
     name,
     gender,
@@ -46,8 +50,20 @@ app.post("/submit", upload.none(), (req, res) => {
     q1,
     q2,
     q3,
-    q4
+    q4,
+    image
   } = req.body;
+
+    const imageFileName = `img_${timestamp}.png`;
+
+  // 画像保存
+  if (image) {
+    const base64Data = image.replace(/^data:image\/png;base64,/, "");
+    const imagePath = path.join(__dirname, "images", imageFileName);
+    fs.writeFile(imagePath, base64Data, "base64", (err) => {
+      if (err) console.error("❌ 画像保存エラー:", err);
+    });
+  }
 
   const row = [
     now,
@@ -60,7 +76,8 @@ app.post("/submit", upload.none(), (req, res) => {
     q1,
     q2,
     q3,
-    q4
+    q4,
+    imageFileName
   ];
 
   const escaped = row.map(value => {
